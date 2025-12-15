52a8f60fd7cdd14212485bb9cba35478bcae1e8c
falibabaei <66964597+falibabaei@users.noreply.github.com>
2025-12-15 09:31:06 +0000

    Modify job submission for multi-gpu training
    Updated GPU allocation and added environment variable exports for CUDA and SLURM job configuration.

--- job_submission.sh
+++ job_submission.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 #SBATCH --partition=normal
 #SBATCH --time=5:00:00
-#SBATCH --gres=gpu:0
+#SBATCH --gres=gpu:2     
+#SBATCH --cpus-per-task=4
 
+module load devel/cuda/12.9
+source "$HOME"/nvflare/bin/activate
+cd "$HOME"/basic_training_diffusion_mlflow_perun/
 
-source "$HOME"/.bashrc
-echo 'Starting script'
-source .env  
+echo "SLURM_JOB_GPUS: $SLURM_JOB_GPUS"
 
-source my_env/bin/activate
+export CUDA_VISIBLE_DEVICES=$SLURM_JOB_GPUS
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 
-python basic_training_diffusion.py
+source .env
+export GPUS_PER_NODE=2
+echo "-----------------------------------------------------------"
+nvidia-smi -l 3 >> nvidia-smi-${SLURM_JOB_ID}.log &
+nvidia-smi --list-gpus
+
+######################
+
+export LAUNCHER="accelerate launch \
+    --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
+    --num_machines $SLURM_NNODES \
+    "
+ 
+export SCRIPT="basic_training_diffusion.py"
+
+    
+# This step is necessary because accelerate launch does not handle multiline arguments properly
+export CMD="$LAUNCHER $SCRIPT " 
+srun $CMD
+ 
